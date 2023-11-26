4f4263476bf7529ecbc57c6ebd61289295b2957a
EsmiK <kratkypaul@seznam.cz>
2023-11-26 17:06:05 +0100

    walked back on api groups

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [selectedClass, setSelectedClass] = useState("");
-  const [timetable, setTimetable] = useState(null);
-  const [groups, setGroups] = useState(null);
+  const [result, setResult] = useState(null);
   const [selectOptionsHTML, setSelectOptionsHTML] = useState("");
+  const [selectElements, setSelectElements] = useState([]); // Declare selectElements
+  const [showGroupsForm, setShowGroupsForm] = useState(false);
+  const [checkboxes, setCheckboxes] = useState({});
+  const [selectedGroup, setSelectedGroup] = useState("");
 
   const handleSelectChange = (e) => {
     setSelectedClass(e.target.value);
@@ -37,7 +40,7 @@ function App() {
   const fetchTimetable = async () => {
     try {
       const response = await fetch(
-        "/api/fetch-timetable",
+        "http://localhost:3001/api/fetch-timetable",
         {
           method: "POST",
           headers: {
@@ -47,40 +50,108 @@ function App() {
         }
       );
 
+      function separateArrays(inputArray) {
+        const result = {};
+      
+        inputArray.forEach(item => {
+          const key = item.match(/[A-Z]+/)[0];
+          if (!result[key]) {
+            result[key] = [];
+          }
+      
+          result[key].push(item);
+        });
+      
+        // Separate 'DV' and 'CM'
+        result['special'] = [];
+        inputArray.forEach(item => {
+          if (item === 'DV' || item === 'CM') {
+            result['special'].push(item);
+          }
+        });
+      
+        return result;
+      }
+    
+  
       if (response.ok) {
         const data = await response.json();
-        setTimetable(data);
-        console.log(data);
+        const selectGroup = [];
+        const checkboxGroup = [];
+        const groups = [];
+  
+        for (const day of data.timetable) {
+          for (const lessons of Object.values(day)) {
+            for (const lesson of lessons) {
+              const group = lesson.group;
+              if (group) {
+                  groups.push(group);
+                }
+              }
+            }
+          }
+        }
+        const uniqueGroups = [...new Set(groups)];
+
+        uniqueGroups = separateArrays(uniqueGroups);
+
+        console.log(uniqueGroups);
+  
+        const selectElements = [];
+
+        for (const key in uniqueGroups) {
+          console.log(uniqueGroups[key]);
+          if (uniqueSelectGroup.includes(uniqueGroups[key])) {
+            const options = uniqueGroups[key].map((lesson) => (
+              <option key={lesson.id} value={lesson.group}>
+                {lesson.group}
+              </option>
+            ));
+            selectElements.push(
+              <Col key={key}>
+                <Form.Select onChange={handleGroupSelectChange}>
+                  <option value="" disabled>
+                    Select a group
+                  </option>
+                  {options}
+                </Form.Select>
+              </Col>
+            );
+          } else {
+            const checkbox = (
+              <Col key={key}>
+                <Form.Check
+                  type="checkbox"
+                  label={uniqueGroups[key]}
+                  checked={checkboxes[key]}
+                  onChange={() => handleCheckboxChange(key)}
+                />
+              </Col>
+            );
+            selectElements.push(checkbox);
+          }
+        }        
+  
+        setResult(data);
+        setShowGroupsForm(true);
+        setSelectElements(selectElements);
       } else {
-        console.error("Failed to fetch timetable data");
+        console.error("Failed to fetch data");
       }
     } catch (error) {
       console.error(error);
     }
+  };  
+
+  const handleGroupSelectChange = (e) => {
+    setSelectedGroup(e.target.value);
   };
 
-  const fetchGroups = async () => {
-    try {
-      const response = await fetch(
-        "/api/fetch-groups",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ class: selectedClass }),
-        }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        setGroups(data);
-      } else {
-        console.error("Failed to fetch groups");
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  const handleCheckboxChange = (group) => {
+    setCheckboxes((prevCheckboxes) => ({
+      ...prevCheckboxes,
+      [group]: !prevCheckboxes[group],
+    }));
   };
 
   return (
@@ -98,21 +169,17 @@ function App() {
           <Col>
             <Button onClick={fetchTimetable}>Fetch Timetable</Button>
           </Col>
-          <Col>
-            <Button onClick={fetchGroups}>Fetch Groups</Button>
-          </Col>
         </Row>
+        {showGroupsForm && (
+          <Row className="my-3">
+            {selectElements}
+            <Col>
+              <Button onClick={fetchTimetable}>Fetch Selected Group</Button>
+            </Col>
+          </Row>
+        )}
       </div>
-      <div>
-        <div>
-          <h3>Groups:</h3>
-          <pre>{JSON.stringify(groups, null, 2)}</pre>
-        </div>
-        <div>
-          <h3>Complete Timetable:</h3>
-          <pre>{JSON.stringify(timetable, null, 2)}</pre>
-        </div>
-      </div>
+      {result && <pre className="my-3">{JSON.stringify(result, null, 2)}</pre>}
     </div>
   );
 }
